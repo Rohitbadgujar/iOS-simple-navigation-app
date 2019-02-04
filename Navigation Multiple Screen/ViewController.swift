9bb67e9b80fb161cb2aae94f5594f849e0f2426a
Rohit Shivdas Badgujar <rsb5@cs1-15-09.local>
2019-02-04 17:31:42 +0000

    Initial Commit

---
+++ Navigation Multiple Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation Multiple Screen
+//
+//  Created by Rohit Shivdas Badgujar on 04/02/2019.
+//  Copyright © 2019 Rohit Shivdas Badgujar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Navigation Multiple ScreenTests/Navigation_Multiple_ScreenTests.swift
@@ -0,0 +1,34 @@
+//
+//  Navigation_Multiple_ScreenTests.swift
+//  Navigation Multiple ScreenTests
+//
+//  Created by Rohit Shivdas Badgujar on 04/02/2019.
+//  Copyright © 2019 Rohit Shivdas Badgujar. All rights reserved.
+//
+
+import XCTest
+@testable import Navigation_Multiple_Screen
+
+class Navigation_Multiple_ScreenTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
